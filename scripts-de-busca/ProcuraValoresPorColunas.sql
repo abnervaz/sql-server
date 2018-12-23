5a91839b4bea442614ad266affd5b2610e6aafed
Abner Vaz <abnervaz@icloud.com>
2018-12-23 17:21:48 -0200

    Update ProcuraValoresPorColunas.sql

--- scripts-de-busca/ProcuraValoresPorColunas.sql
+++ scripts-de-busca/ProcuraValoresPorColunas.sql
@@ -1,7 +1,17 @@
+/************************************************************************
+*	Description: Procura em todas as tabelas um determinado	valor		
+*		     de uma determinada coluna								
+*																		
+*	Parameters.: @column => Especifica o nome da coluna					
+*		     @operator => Especifica o operador de comparação		
+*		     @value => Especifica o valor para a coluna definida	
+*																		
+*************************************************************************/
+
     DECLARE @tables		TABLE(name varchar(100))
 	DECLARE @result		TABLE(Tabela varchar(100))
-	DECLARE @column		VARCHAR(20) = 'IDORGANIZACAO',
-			@operador	CHAR(2) = '<>',
+	DECLARE @column		VARCHAR(20) = '',
+			@operator	CHAR(2) = '<>',	
 			@value		VARCHAR(20) = '1'
 
 INSERT INTO @tables
@@ -16,7 +26,7 @@ WHILE(EXISTS(SELECT * FROM @tables))
 				@query NVARCHAR(MAX)
 		
 		 SELECT @table = MIN(name) FROM @tables
-		 SELECT @query = 'SELECT TOP 1 ''' + @table + ''' FROM ' + @table + ' WHERE ' + @column + @operador + @value
+		 SELECT @query = 'SELECT TOP 1 ''' + @table + ''' FROM ' + @table + ' WHERE ' + @column + @operator + @value
 
 		INSERT INTO @result
 		EXEC sys.sp_executesql @query
@@ -24,5 +34,4 @@ WHILE(EXISTS(SELECT * FROM @tables))
 		DELETE FROM @tables WHERE name = @table		
 	END
 
-  SELECT * 
-   FROM @result
+  SELECT * FROM @result
