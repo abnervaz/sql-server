8b99101a0ff52ad4ba9ea7d385dadf22adc3b265
Abner Vaz <abnervaz@icloud.com>
2018-12-23 17:04:07 -0200

    Add files via upload

---
+++ scripts-de-busca/Procura tabela em todas as bases.sql
@@ -0,0 +1,41 @@
+DECLARE @object varchar(200) = 'idUsuario'
+DECLARE @dataBases	TABLE(name varchar(100), row int)
+DECLARE @result TABLE (
+	[database] varchar(50),
+	[object] varchar(200),
+	[type] varchar(200)
+)
+DECLARE @row		INT
+
+INSERT INTO @dataBases
+SELECT b.name, ROW_NUMBER () OVER(ORDER BY b.database_id) AS r 
+  FROM sys.databases AS b 
+ WHERE b.state = 0
+
+SELECT @row = MAX(row) FROM @dataBases
+
+WHILE @row > 0
+BEGIN
+	
+		DECLARE @command NVARCHAR(1000)	
+		SELECT
+			@command = 
+			'USE ' + name + '
+			SELECT ''' + name + ''', 
+				s.name + ''.'' + o.name AS object
+			   ,o.type_desc
+			FROM sys.objects AS o
+			JOIN sys.all_sql_modules AS m
+				ON m.object_id = o.object_id
+			JOIN sys.schemas AS s
+			ON s.schema_id = o.schema_id
+			WHERE m.definition LIKE ''%' + @object + '%'''
+				FROM @dataBases WHERE row = @row
+
+		INSERT INTO @result
+		EXEC sys.sp_executesql @command
+
+	SELECT @row = @row - 1
+ END
+
+SELECT * FROM @result
---
+++ scripts-de-busca/Procura uma proc em todos os DBs.sql
@@ -0,0 +1,44 @@
+DECLARE @Procedure VARCHAR(100) = 'UsuariosTrilhas_Processar' -- PARÂMETRO
+/*******************************************************************
+********************************************************************
+********************************************************************/
+DECLARE @dataBases	TABLE(name varchar(100), row int)
+DECLARE @result TABLE (
+	[database] varchar(50)
+)
+DECLARE @row		INT
+
+INSERT INTO @dataBases
+SELECT b.name, ROW_NUMBER () OVER(ORDER BY b.database_id) AS r 
+  FROM sys.databases AS b 
+ WHERE b.collation_name = 'Latin1_General_CI_AS' AND b.state = 0
+
+SELECT @row = MAX(row) FROM @dataBases
+
+WHILE @row > 0
+BEGIN
+	
+		DECLARE @command NVARCHAR(1000)	
+		SELECT
+			@command = 
+			'BEGIN USE ' + name + ' IF(EXISTS(SELECT 1 FROM sys.tables AS T JOIN sys.schemas AS S ON S.schema_id = T.schema_id WHERE S.name = ''Usuarios'' AND T.name = ''Usuarios''))' +
+			'BEGIN
+			SELECT
+					''' + name + ''' 
+					WHERE NOT EXISTS(
+					SELECT M.object_id
+					FROM sys.all_sql_modules AS M
+					JOIN sys.procedures AS P
+						ON M.object_id = P.object_id
+					WHERE P.name = ''' + @Procedure + ''')			
+				END	
+			END'
+				FROM @dataBases WHERE row = @row
+
+		INSERT INTO @result
+		EXEC sys.sp_executesql @command
+
+	SELECT @row = @row - 1
+ END
+
+SELECT * FROM @result
